5ad2468de94a5e96d5e4b3921bd8859238b10bd9
aasthasona79 <aasthasona79@gmail.com>
2018-08-30 17:57:01 +0530

    php files

---
+++ footer.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <style type="text/css">
+    .links{
+      color:white;
+
+    }
+    footer-links a{
+      color:white;
+    }
+    @media screen and  (min-width: 1401px) {
+    footer{
+    margin-right: 0rem;
+    margin-left: 0rem;
+    padding-left: 5em;
+    padding-right: 5em;
+    display: block;
+    max-width: none !important;
+   }
+  }
+  @media screen and (min-width: 481px) and (max-width: 767px){
+    .footer-links{
+    padding-top: 25px;
+    text-align: center !important;
+    }
+  } 
+  @media screen and (max-width: 481px){
+   
+    .footer-links{
+    padding-top: 25px;
+    text-align: center;
+
+    }
+    .footer-links-2{
+      text-align: center !important;
+    }
+    .footer-desc{
+      padding-left: 0px !important;
+      margin-right: 0px !important;
+    }
+  }
+  </style>
+  
+  <title></title>
+</head>
+<body>
+  <footer class="row responsive" style="background-color: #F0663B;font-size: 13.5px;margin-left: 0;margin-right: 0;max-width: 100%;"><br>
+          <div class="row align-center grid-x footer-desc" id="footrow" style="margin-bottom: 5%;padding-left: 5%;padding-right: 0;margin-right: 0 !important;max-width: 100%;">
+            <div class="large-3 small-12 medium-3 columns size footer-links">
+              <h6><b>Quick Links</b></h6>
+              <a href="https://www.lnmiit.ac.in/" class="links">LNMIIT Home Page</a><br>
+              <a href="recruiters.html" class="links">Recruiters </a><br>
+              <a href="cons_stats.html" class="links">Statistics</a><br>
+            </div>
+            <div class="large-3 small-12 medium-3 columns size footer-links">
+              <h6><b>Download</b></h6>
+              <a href="#footrow" class="links">Placement Brochure</a><br>
+            </div>
+            <div class="large-3 small-12 medium-3 columns size footer-links">
+              <h6><b>Contact</b></h6>
+              Placement Office<br>
+              Guest House<br>
+              The LNM Institute of Information & Technology<br>
+              Rupa ki Nangal, Post Sumel, Via Jamdoli<br>
+              Jaipur-302031</a>
+            </div>
+            <div class="large-3 small-12 medium-3 columns size footer-links">
+              
+              <img src="images/mail_icon.png" style="height: 1.2em"> <a href="mailto:tpo@lnmiit.ac.in" style="color: white;">tpo@lnmiit.ac.in</a><br>
+              <img src="images/phone-icon-orange.png" style="height: 1.5em">+91 141-2689015, 8696792550
+            </div>
+          </div>
+
+          <hr style="color: white;max-width: 100%;">
+           <div class="row align-center grid-x size footer-links-2" style="padding-left: 0;padding-right: 0;margin-right: 0 !important;max-width: 100%;margin-left: 0;">
+          <div class="large-4 columns">
+                <p> ©Copyright 2018. All rights reserved | Placement Cell LNMIIT</p>
+          </div>
+           <div class="large-8 columns">
+             <p align="right" class="footer-links-2"> Developers:- <a href="https://github.com/jitenss" target="blank" class="links"> Jiten Sadhwani</a>,<a href="https://github.com/   Vedarth" target="blank" class="links"> Vedarth Sharma</a>,<a href="https://github.com/aasthasona79" target="blank" class="links"> Aastha Agarwal</a>,<a href="https://github.com/kritika0598" target="blank" class="links"> Kritika singhal</a>,<a href="https://github.com/tusharb104" target="blank" class="links"> Tushar Bansal</a>,<a href="https://github.com/atharvasharma"  target="blank" class="links"> Atharva Sharma</a>,<a href="https://github.com/NickSneo" target="blank" class="links"> Nischal Sharma</a>
+               </p>
+          </div>
+         </div>
+      </footer>
+
+</body>
+</html>
